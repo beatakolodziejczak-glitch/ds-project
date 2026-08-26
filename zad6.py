0a91239b7eabb96c25af396af9217ffe605789c6
Beata <beata.kolodziejczak@gmail.com>
2026-08-26 21:05:40 +0200

    Dodanie rozwiazania zadania 6

---
+++ zad6.py
@@ -0,0 +1,7 @@
+accuracy = 0.9234
+precision = 0.8891
+recall = 0.9456
+
+print(f"{"Accuracy":<12}: {accuracy:.2%}")
+print(f"{"Precision":<12}: {precision:.2%}")
+print(f"{"Recall":<12}: {recall:.2%}")
